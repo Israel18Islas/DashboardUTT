ad756d05926e479ef6fa3c5b8042fe848d5bd0e7
Naye2218 <0319123803@ut-tijuana.edu.mx>
2022-09-22 13:51:23 -0700

    implementacion vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -28,7 +28,7 @@
             <li><a href="#">Contact</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logins</a></li>
         </ul>
         </div>
     </div>
