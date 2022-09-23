5d8723e5c1e8ac5a91176064cd93e6afe09a75bb
Naye2218 <0319123803@ut-tijuana.edu.mx>
2022-09-23 15:39:59 -0700

    actualizar pantalla

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
-import bootstrap from 'bootstrap'
+require('./bootstrap');
 
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
+import {Tabs, Tab} from 'vue3-tabs-component';
  
 InertiaProgress.init();
 
@@ -10,6 +11,8 @@ createInertiaApp({
     resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
+            .component('tabs', Tabs)
+            .component('tab', Tab)
             .use(plugin)
             .mount(el);
     },
